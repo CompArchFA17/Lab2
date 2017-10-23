78bbf21bc915d79f5a2ca2772299f2d8f23f26ce
arianaolson419 <arianaolson419@gmail.com>
2017-10-23 18:53:10 -0400

    Implement and test rising and failling output edges

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -2,6 +2,8 @@
 // Input Conditioner test bench
 //------------------------------------------------------------------------
 
+`include "inputconditioner.v"
+
 module testConditioner();
 
     reg clk;
@@ -11,19 +13,43 @@ module testConditioner();
     wire falling;
     
     inputconditioner dut(.clk(clk),
-    			 .noisysignal(pin),
+    		 .noisysignal(pin),
 			 .conditioned(conditioned),
 			 .positiveedge(rising),
-			 .negativeedge(falling))
+			 .negativeedge(falling));
 
 
     // Generate clock (50MHz)
-    initial clk=0;
-    always #10 clk=!clk;    // 50MHz Clock
+    initial clk = 0;
+    always #10 clk =! clk;    // 50MHz Clock
     
     initial begin
-    // Your Test Code
-    // Be sure to test each of the three conditioner functions:
-    // Synchronization, Debouncing, Edge Detection
+        $dumpfile("input_conditioner.vcd");
+        $dumpvars();
+
+        pin = 0; #300
+        pin = 1; #300
+        pin = 0; #300
+        pin = 1; #300
+        pin = 0; #300
+        pin = 1; #300
+        pin = 0; #300
+        $finish();
+    end
+
+    // Test Case 3: Positive Edge Detection
+    always @(posedge conditioned) begin
+        if (rising != 1) begin
+            $display("Test Case 3 failed: rising edge not detected at time %t", $time);
+            $display("rising: %b", rising);
+        end
+    end
     
+    // Test Case 4: Negative Edge Detection
+    always @(negedge conditioned) begin
+        if (falling != 1 && $time > 100) begin
+            $display("Test Case 4 failed: falling edge not detected at time %t", $time);
+            $display("falling: %b", falling);
+        end
+    end
 endmodule
