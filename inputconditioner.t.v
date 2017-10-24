e6150710bf2f46942fd0b49a983ac4d76d5d237f
mjakus <margaret.jakus@students.olin.edu>
2017-10-23 20:11:49 -0400

    Never fear, GTKWave is fine

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -25,37 +25,42 @@ module testConditioner();
 $dumpfile("inputconditioner.vcd");
 $dumpvars();
 
-// $display(" clk | pin | conditioned | -edge| + edge"); 
-// pin = 0; #50
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 1; #80
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 0; #2
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 1; #70
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 0; #90
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 1; #8
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 0; #70
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 1; #150
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 0; #50
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 1; #15
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 0; #1
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 1; #70
-// $display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
-// pin = 0; #50
+$display(" clk | pin | conditioned | -edge| + edge"); 
+pin = 0; #50
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 1; #80
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 0; #12
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 1; #70
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 0; #90
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 1; #8
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 0; #70
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 1; #150
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 0; #50
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 1; #15
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 0; #1
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 1; #70
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 0; #50
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 1; #5
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
+pin = 0; #280
+$display("%b | %b | %b | %b | %b", clk, pin, conditioned, rising, falling );
 
 // something shiesty is going on with the passing from synchronizer 1 to the conditioned signal.
 // I'll try to take a look at it again before we meet. 
 // See GTK wave simulation with these paramters to observe jankness with your own eyes. 
-
+/*
 pin = 0; #50
 pin = 1; #160
 pin = 0; #70
@@ -69,6 +74,7 @@ pin = 0; #100
 pin = 1; #70
 
 pin = 0; #150
+*/
 // pin = 1; #50
 // pin = 0; #15
 // pin = 1; #1
