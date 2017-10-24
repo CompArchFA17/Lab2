12636ca7becd2a78d8a8354261d94d6b45bfd863
Jonah Spear <Jonahspear512@gmail.com>
2017-10-24 16:49:21 -0400

    unmess up stuff

--- inputconditioner.v
+++ inputconditioner.v
@@ -16,13 +16,13 @@ output reg  negativeedge    // 1 clk pulse at falling edge of conditioned
 
     parameter counterwidth = 3; // Counter size, in bits, >= log2(waittime)
     parameter waittime = 3;     // Debounce delay, in clock cycles
-    
-    reg[counterwidth-1:0] counter = 0; // 0
-    reg synchronizer0 = 0; // 1
-    reg synchronizer1 = 0; // 1
-    // conditioned <= 0;
-    
-    // 0 0 0 0 1 0 1 1 1 1 1 
+
+    reg[counterwidth-1:0] counter = 0;
+    reg prevconditioned = 0;
+    reg synchronizer0 = 0;
+    reg synchronizer1 = 0;
+
+    //0 0 0 0 1 0 1 1 1 1
     always @(posedge clk ) begin
         // Case 1: previous signal is same as current output
         if(conditioned == synchronizer1)
