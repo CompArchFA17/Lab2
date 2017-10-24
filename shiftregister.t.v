9af0dc680cba04ee05450d8d444859765eb64619
Henry Rachootin <henry.rachootin@students.olin.edu>
2017-10-24 11:33:55 -0400

    made a test bench, fixed the shift resgister

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -1,3 +1,4 @@
+`include "shiftregister.v"
 //------------------------------------------------------------------------
 // Shift Register test bench
 //------------------------------------------------------------------------
@@ -10,20 +11,94 @@ module testshiftregister();
     wire[7:0]       parallelDataOut;
     wire            serialDataOut;
     reg[7:0]        parallelDataIn;
-    reg             serialDataIn; 
-    
+    reg             serialDataIn;
+
     // Instantiate with parameter width = 8
-    shiftregister #(8) dut(.clk(clk), 
+    shiftregister #(8) dut(.clk(clk),
     		           .peripheralClkEdge(peripheralClkEdge),
-    		           .parallelLoad(parallelLoad), 
-    		           .parallelDataIn(parallelDataIn), 
-    		           .serialDataIn(serialDataIn), 
-    		           .parallelDataOut(parallelDataOut), 
+    		           .parallelLoad(parallelLoad),
+    		           .parallelDataIn(parallelDataIn),
+    		           .serialDataIn(serialDataIn),
+    		           .parallelDataOut(parallelDataOut),
     		           .serialDataOut(serialDataOut));
-    
+    reg dutpassed;
+
     initial begin
-    	// Your Test Code
+      $dumpfile("shift.vcd");
+      $dumpvars(0, testshiftregister);
+      //initialize
+      dutpassed = 1;
+      clk = 0; #5;
+    	parallelDataIn = 8'd0;
+      parallelLoad   = 1;
+      clk = 1; #5;
+      clk = 0; #5;
+      if(parallelDataOut != 8'd0 || ^parallelDataOut === 1'bx) begin
+          $display("initialization failed, parallel");
+          dutpassed = 0;
+      end
+      if(serialDataOut != 0 || serialDataOut === 1'bx) begin
+          $display("initialization failed, serial");
+          dutpassed = 0;
+      end
+
+      //serialLoad some bits
+      parallelLoad = 0;
+      serialDataIn = 1;
+      clk = 1; #5; clk = 0; #5;
+      serialDataIn = 1;
+      clk = 1; #5; clk = 0; #5;
+      serialDataIn = 0;
+      clk = 1; #5; clk = 0; #5;
+      serialDataIn = 0;
+      clk = 1; #5; clk = 0; #5;
+      serialDataIn = 1;
+      clk = 1; #5; clk = 0; #5;
+      serialDataIn = 0;
+      clk = 1; #5; clk = 0; #5;
+      serialDataIn = 1;
+      clk = 1; #5; clk = 0; #5;
+      serialDataIn = 0;
+      clk = 1; #5; clk = 0; #5;
+      if(parallelDataOut != 8'b11001010 || ^parallelDataOut === 1'bx) begin
+          $display("Serial in failed, parallel");
+          dutpassed = 0;
+      end
+      if(serialDataOut != 1 || serialDataOut === 1'bx) begin
+          $display("Serial in failed, serial");
+          dutpassed = 0;
+      end
+
+      //parallelLoad some bits
+      parallelLoad = 1;
+      parallelDataIn = 8'b01010011;
+      clk = 1; #5; clk = 0; #5;
+      if(parallelDataOut != 8'b01010011 || ^parallelDataOut === 1'bx) begin
+          $display("Parallel in failed, parallel");
+          dutpassed = 0;
+      end
+      if(serialDataOut != 0 || serialDataOut === 1'bx) begin
+          $display("Parallel in failed, serial");
+          dutpassed = 0;
+      end
+
+      //parallel and serial at the same time
+      parallelLoad = 1;
+      serialDataIn = 0;
+      parallelDataIn = 8'b11111111;
+      clk = 1; #5; clk = 0; #5;
+      if(parallelDataOut != 8'b11111111 || ^parallelDataOut === 1'bx) begin
+          $display("Same time in failed, parallel");
+          dutpassed = 0;
+      end
+      if(serialDataOut != 1 || serialDataOut === 1'bx) begin
+          $display("Same time in failed, serial");
+          dutpassed = 0;
+      end
+
+      if(dutpassed) begin
+          $display("DUT passed!");
+      end
+      $finish();
     end
-
 endmodule
-
--- shiftregister.v
+++ shiftregister.v
@@ -14,19 +14,19 @@ input               peripheralClkEdge,  // Edge indicator
 input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
 input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
 input               serialDataIn,       // Load shift reg serially
-output reg [width-1:0]  parallelDataOut,    // Shift reg data contents
-output reg             serialDataOut       // Positive edge synchronized
+output [width-1:0]  parallelDataOut,    // Shift reg data contents
+output              serialDataOut       // Positive edge synchronized
 );
 
     reg [width-1:0]      shiftregistermem;
     always @(posedge clk) begin
-        if(parallelLoad)
-	  shiftregistermem <= parallelDataIn;
-	else begin
-	   shiftregistermem <= {shiftregistermem[width-2:0],serialDataIn};
-	end
-       serialDataOut <= shiftregistermem[width-1];
-       parallelDataOut <= shiftregistermem;
-       
+      if(parallelLoad)
+  	     shiftregistermem <= parallelDataIn;
+    	else begin
+    	   shiftregistermem <= {shiftregistermem[width-2:0],serialDataIn};
+    	end
     end
+
+    assign parallelDataOut = shiftregistermem;
+    assign serialDataOut   = shiftregistermem[width-1];
 endmodule
