10d7dbbbd01fa5d7b7882416ba1117df0abbd49f
Tobias Shapinsky <tobiasshapinsky@gmail.com>
2017-10-24 11:04:59 -0400

    defined shift-register

--- shiftregister.v
+++ shiftregister.v
@@ -14,12 +14,19 @@ input               peripheralClkEdge,  // Edge indicator
 input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
 input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
 input               serialDataIn,       // Load shift reg serially
-output [width-1:0]  parallelDataOut,    // Shift reg data contents
-output              serialDataOut       // Positive edge synchronized
+output reg [width-1:0]  parallelDataOut,    // Shift reg data contents
+output reg             serialDataOut       // Positive edge synchronized
 );
 
     reg [width-1:0]      shiftregistermem;
     always @(posedge clk) begin
-        // Your Code Here
+        if(parallelLoad)
+	  shiftregistermem <= parallelDataIn;
+	else begin
+	   shiftregistermem <= {shiftregistermem[width-2:0],serialDataIn};
+	end
+       serialDataOut <= shiftregistermem[width-1];
+       parallelDataOut <= shiftregistermem;
+       
     end
 endmodule
