19504a5045bb8c9c57ca0f92705a45eec3daaabd
Prava Dhulipalla <prava@students.olin.edu>
2017-10-24 15:31:35 -0400

    finished shift register, basic tests complete

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -2,6 +2,8 @@
 // Shift Register test bench
 //------------------------------------------------------------------------
 
+`include "shiftregister.v"
+
 module testshiftregister();
 
     reg             clk;
@@ -21,8 +23,53 @@ module testshiftregister();
     		           .parallelDataOut(parallelDataOut), 
     		           .serialDataOut(serialDataOut));
     
+    initial clk = 0;
+    always #10 clk = !clk;    // 50MHz Clock
+
+
+    // Basic test for Serial In, Parallel Out.
     initial begin
-    	// Your Test Code
+        $dumpfile("shiftregister.vcd");
+        $dumpvars();
+
+        parallelLoad = 0; 
+        peripheralClkEdge = 0;
+        serialDataIn = 0;   #50
+        peripheralClkEdge = 1; #10
+        peripheralClkEdge = 0;
+        serialDataIn = 1;   #50
+        peripheralClkEdge = 1; #10
+        peripheralClkEdge = 0;
+        serialDataIn = 1;   #50
+        peripheralClkEdge = 1; #10
+        peripheralClkEdge = 0;
+        serialDataIn = 1;   #50
+        peripheralClkEdge = 1; #10
+        peripheralClkEdge = 0;
+        serialDataIn = 0;   #50 
+        peripheralClkEdge = 1; #10
+        peripheralClkEdge = 0;      
+        serialDataIn = 1;   #50 
+        peripheralClkEdge = 1; #10
+        peripheralClkEdge = 0;       
+        serialDataIn = 0;   #50
+        peripheralClkEdge = 1; #10
+        peripheralClkEdge = 0;
+        serialDataIn = 1;   #50
+
+        $displayb(parallelDataOut);
+
+        $finish();
+    end
+
+    // Basic test for Parallel In, Serial Out
+    initial begin
+
+        parallelLoad = 1;
+        parallelDataIn = 8'b01010101; #50
+
+        $display(serialDataOut);
+
     end
 
 endmodule
--- shiftregister.v
+++ shiftregister.v
@@ -6,20 +6,30 @@
 //      - parallel in, serial out
 //------------------------------------------------------------------------
 
-module shiftregister
-#(parameter width = 8)
-(
-input               clk,                // FPGA Clock
-input               peripheralClkEdge,  // Edge indicator
-input               parallelLoad,       // 1 = Load shift reg with parallelDataIn
-input  [width-1:0]  parallelDataIn,     // Load shift reg in parallel
-input               serialDataIn,       // Load shift reg serially
-output [width-1:0]  parallelDataOut,    // Shift reg data contents
-output              serialDataOut       // Positive edge synchronized
+module shiftregister #(parameter width = 8) (
+	input                  clk, 				// FPGA Clock
+	input                  peripheralClkEdge, 	// Edge indicator
+	input                  parallelLoad, 		// 1 = Load shift reg with parallelDataIn
+	input      [width-1:0] parallelDataIn, 		// Load shift reg in parallel
+	input                  serialDataIn, 		// Load shift reg serially
+	output reg [width-1:0] parallelDataOut, 	// Shift reg data contents
+	output reg             serialDataOut     	// Positive edge synchronized
 );
 
-    reg [width-1:0]      shiftregistermem;
-    always @(posedge clk) begin
-        // Your Code Here
-    end
+	reg [width-1:0]      shiftregistermem;
+
+	always @(posedge clk) begin
+		if (parallelLoad) begin
+			shiftregistermem <= parallelDataIn;
+		end
+		parallelDataOut <= shiftregistermem;
+		serialDataOut <= shiftregistermem[width-1];
+	end
+
+	// when peripheral clock has an edge, shift register advances one position and serialIn is loaded into LSB
+	always @(peripheralClkEdge) begin
+		shiftregistermem <= parallelDataOut;
+		parallelDataOut <= {shiftregistermem[width-2:0], serialDataIn};
+	end
+
 endmodule
