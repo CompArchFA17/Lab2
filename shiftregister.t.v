a8494e8558d15570ed462512be7580503d21657d
dpapp <david.papp@students.olin.edu>
2017-10-25 16:27:57 -0400

    serial tests

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -1,6 +1,7 @@
 //------------------------------------------------------------------------
 // Shift Register test bench
 //------------------------------------------------------------------------
+`include "shiftregister.v"
 
 module testshiftregister();
 
@@ -21,8 +22,46 @@ module testshiftregister();
     		           .parallelDataOut(parallelDataOut), 
     		           .serialDataOut(serialDataOut));
     
+    always #10 clk=!clk; 
     initial begin
-    	// Your Test Code
+        //$dumpfile("shiftregister.vcd");
+        //$dumpvars;
+        parallelLoad = 1;
+        parallelDataIn = 8'b00000000; #100
+        peripheralClkEdge = 1;
+
+        serialDataIn = 1; #10
+        if (serialDataOut == 0) begin
+            $display("Passed Test Case 1");
+        end
+        else begin
+            $display("Failed Test Case 1");
+            $display(serialDataOut);
+            $display("%8b", parallelDataOut);
+        end
+        if (parallelDataOut == 1) begin
+            $display("Failed Test Case 2");
+        end
+        else begin
+            $display("Passed Test Case 2");
+        end
+        #30
+        if (serialDataOut != 0) begin
+            $display("Failed Test Case 3");
+        end
+        if (parallelDataOut != 8) begin
+            $display("Failed Test Case 4");
+        end
+        #50
+        /*if (parallelLoad == 0) begin
+            $display("Failed!");
+        end
+        serialDataIn = 0; #10
+        serialDataIn = 1; #10
+        serialDataIn = 1; #10
+        serialDataIn = 1; 
+        #100*/
+        $finish;
     end
 
 endmodule
