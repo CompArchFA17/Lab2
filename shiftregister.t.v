feebecbf43d0358331dc41f9a4d909d4ad6b38b1
Jonah Spear <Jonahspear512@gmail.com>
2017-10-25 16:19:33 -0400

    Add parallel tests

--- shiftregister.t.v
+++ shiftregister.t.v
@@ -1,6 +1,7 @@
 //------------------------------------------------------------------------
 // Shift Register test bench
 //------------------------------------------------------------------------
+`include "shiftregister.v"
 
 module testshiftregister();
 
@@ -10,20 +11,63 @@ module testshiftregister();
     wire[7:0]       parallelDataOut;
     wire            serialDataOut;
     reg[7:0]        parallelDataIn;
-    reg             serialDataIn; 
-    
+    reg             serialDataIn;
+
     // Instantiate with parameter width = 8
-    shiftregister #(8) dut(.clk(clk), 
+    shiftregister #(8) dut(.clk(clk),
     		           .peripheralClkEdge(peripheralClkEdge),
-    		           .parallelLoad(parallelLoad), 
-    		           .parallelDataIn(parallelDataIn), 
-    		           .serialDataIn(serialDataIn), 
-    		           .parallelDataOut(parallelDataOut), 
+    		           .parallelLoad(parallelLoad),
+    		           .parallelDataIn(parallelDataIn),
+    		           .serialDataIn(serialDataIn),
+    		           .parallelDataOut(parallelDataOut),
     		           .serialDataOut(serialDataOut));
-    
+
+    // Generate clock (50MHz)
+    initial clk=0;
+    always #5 clk=!clk;
+
     initial begin
-    	// Your Test Code
+        // Test parallel looads
+    	parallelLoad = 1;
+        parallelDataIn = 8'b00000000;
+        #20;
+        if (parallelDataOut == 8'b00000000) begin
+            $display("Test Case 1 Passed");
+        end
+        else begin
+            $display("Test Case 1 Failed!");
+        end
+
+        parallelDataIn = 8'b00001111;
+        #20;
+        if (parallelDataOut == 8'b00001111) begin
+            $display("Test Case 2 Passed");
+        end
+        else begin
+            $display("Test Case 2 Failed!");
+        end
+
+        parallelDataIn = 8'b11111111;
+        #20;
+        if (parallelDataOut == 8'b11111111) begin
+            $display("Test Case 3 Passed");
+        end
+        else begin
+            $display("Test Case 3 Failed!");
+        end
+
+        parallelLoad = 0;
+        parallelDataIn = 8'b00000000;
+        #20;
+        if (parallelDataOut == 8'b00000000) begin
+            $display("Test Case 4 Failed!");
+        end
+        else begin
+            $display("Test Case 4 Passed");
+        end
+
+
+        $finish;
     end
 
 endmodule
-
