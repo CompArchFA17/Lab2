c37885c6d1f5ba9cf10901e76333d4565730b285
apan64 <andrew.pan@students.olin.edu>
2017-10-24 22:36:09 -0400

    input conditioner test done

--- inputconditioner.t.v
+++ inputconditioner.t.v
@@ -1,6 +1,7 @@
 //------------------------------------------------------------------------
 // Input Conditioner test bench
 //------------------------------------------------------------------------
+`timescale 1 ns / 1 ps
 `include "inputconditioner.v"
 
 module testConditioner();
@@ -31,26 +32,37 @@ module testConditioner();
     $display("Input     Time    Conditioned Signal      Clock       Posedge     NegEdge");
     pin = 0;
     #150
-    $display("%b        %d         %b                      %b       %b          %b", pin, 10'd0, conditioned, clk, rising, falling);
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd0, conditioned, clk, rising, falling);
       
     pin = 1;
     #110
-    $display("%b        %d         %b                      %b       %b          %b", pin, 10'd110, conditioned, clk, rising, falling);
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd110, conditioned, clk, rising, falling);
 
     #40
-    $display("%b        %d         %b                      %b       %b          %b", pin, 10'd150, conditioned, clk, rising, falling);
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd150, conditioned, clk, rising, falling);
     
     pin = 0;
     #10
-    $display("%b        %d         %b                      %b       %b          %b", pin, 10'd160, conditioned, clk, rising, falling);
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd160, conditioned, clk, rising, falling);
                
     pin = 1;
     #10
-    $display("%b        %d         %b                      %b       %b          %b", pin, 10'd170, conditioned, clk, rising, falling);
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd170, conditioned, clk, rising, falling);
 
     pin = 0;
     #120
-    $display("%b        %d         %b                      %b       %b          %b", pin, 10'd290, conditioned, clk, rising, falling);
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd290, conditioned, clk, rising, falling);
+
+    #2
+    pin =  1;
+    #100
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd392, conditioned, clk, rising, falling);
+
+    #8
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd400, conditioned, clk, rising, falling);
+
+    #10
+    $display("%b        %d         %b                      %b           %b          %b", pin, 10'd410, conditioned, clk, rising, falling);
       
     #10000 
     $finish;
