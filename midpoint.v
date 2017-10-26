3d9ab426d0a795ca5e7ced6f9a3727ffa67b5d85
Tobias Shapinsky <tobiasshapinsky@gmail.com>
2017-10-25 20:52:15 -0400

    upload to FPGA

--- midpoint.v
+++ midpoint.v
@@ -1,32 +1,34 @@
 `include "shiftregister.v"
 `include "inputconditioner.v"
 module midpoint_wrapper
-  (
+(
    input clk,
    input [3:0] sw,
    input [3:0] btn,
-   input [3:0] led
-   )
+   output [3:0] led
+   );
 
   wire par_load;
    wire ser_in;
    wire clk_edge;
    wire [7:0] par_data_in;
    wire [7:0] par_data_out;
+   reg [3:0]      ledmem;
    
    inputconditioner btn_cond(.clk(clk),.noisysignal(btn[0]),.negativeedge(par_load));
-   inputconditioner sw0_cond(.clk(clk),.noisysignal(sw[0]),.coditioned(ser_in));
+   inputconditioner sw0_cond(.clk(clk),.noisysignal(sw[0]),.conditioned(ser_in));
    inputconditioner sw1_cond(.clk(clk),.noisysignal(sw[1]),.positiveedge(clk_edge));
 
    assign par_data_in = 8'hA5;
 
-   shiftregister reg(.clk(clk),.peripheralClkEdge(clk_edge),.parallelLoad(par_load),.parallelDataIn(par_data_in),.serialDataIn(ser_in),.parallelDataDout(par_data_out));
+   shiftregister shftreg(.clk(clk),.peripheralClkEdge(clk_edge),.parallelLoad(par_load),.parallelDataIn(par_data_in),.serialDataIn(ser_in),.parallelDataOut(par_data_out));
 
    always @(posedge clk)begin
-      if(sw[1])
-	led <= par_data_out[4:7];
+      if(sw[3])
+	   ledmem = par_data_out[7:4];
       else begin
-	 led <= par_data_out[0:3];
+	   ledmem = par_data_out[3:0];
       end
    end
+   assign led=ledmem;
 endmodule
