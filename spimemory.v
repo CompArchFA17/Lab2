cface2bfa30e1c0e01285390818be68e89f67441
Jonah Spear <Jonahspear512@gmail.com>
2017-10-29 19:05:31 -0400

    fixed sr module instantiating

--- spimemory.v
+++ spimemory.v
@@ -11,8 +11,8 @@ module spiMemory
     input           clk,        // FPGA clock
     input           sclk_pin,   // SPI clock
     input           cs_pin,     // SPI chip select
-    output          miso_pin,   // SPI master in slave out
     input           mosi_pin,   // SPI master out slave in
+    output          miso_pin,   // SPI master in slave out
     output [3:0]    leds        // LEDs for debugging
 );
 	reg MISO_BUFE, DM_WE, ADDR_WE, SR_WE;
@@ -22,16 +22,22 @@ module spiMemory
 	inputconditioner condition2(clk, sclk_pin, , positiveEdge, negativeEdge);
 	inputconditioner condition3(clk, cs_pin, conditionedInput2, , );
 
-	wire shiftRegOutP[7:0], serialOut;
-	wire dataMemoryOut[7:0], address[7:0];
-	shiftregister shiftRegister(clk, positiveEdge, SR_WE, dataMemoryOut, conditionedInput1, shiftRegOutP, serialOut);
+	wire[7:0] shiftRegOutP, dataMemoryOut;
+  wire serialOut, address;
 
-	/*dff #(8) dff1(clk, ADDR_WE, shiftRegOutP, address);
-	wire q;
-	dff #(1) dff2(clk, negativeEdge, serialOut, q);
+  shiftregister #(8) sr(.clk(clk),
+                 .peripheralClkEdge(positiveEdge),
+                 .parallelLoad(SR_WE),
+                 .parallelDataIn(dataMemoryOut),
+                 .serialDataIn(conditionedInput1),
+                 .parallelDataOut(shiftRegOutP),
+                 .serialDataOut(serialOut));
 
-	datamemory dataMemory(clk, shiftRegOutP, address, DM_WE, dataMemoryOut);*/
+	// dff #(8) dff1(clk, ADDR_WE, shiftRegOutP, address);
+	// wire q;
+	// dff #(1) dff2(clk, negativeEdge, serialOut, q);
+
+	// datamemory dataMemory(clk, shiftRegOutP, address, DM_WE, dataMemoryOut);
 
 
 endmodule
-   
