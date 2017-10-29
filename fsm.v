aaaa09fbdc6f082bc8c662eae5c3db9c2d57c925
Jonah Spear <Jonahspear512@gmail.com>
2017-10-29 19:34:24 -0400

    add fsm to spi memory

--- fsm.v
+++ fsm.v
@@ -3,14 +3,14 @@
 //Example of a Finite State Machine where the traffic light state dictates the driving.
 
 module finiteStateMachine(
-    input [2:0],
-    input clk,
+    input chip_select,
+    input r_w,
     output reg MISO_BUFE,
     output reg DM_WE,
     output reg ADDR_WE,
     output reg SR_WE
 );
-    
+
 
 reg [7:0] state;
 localparam CSLow = 2'b00;
@@ -20,13 +20,13 @@ localparam CSHigh = 2'b00;
 integer counter = 0;
 //change states on the clk cycles
 always @(posedge clk) begin
-    
+
     // commands: CS_Low, done address load, parallelDataLoad, serialDataLoad
     // CS_high
 
     //normally you'll have an input of strings that'll dictate which states you go to (like the AddressIn)
     //but here, i'll just manually set it
-    //this'll normally be much more confusing and fun. which state should you be in to start? and then generally states lead to other states. 
+    //this'll normally be much more confusing and fun. which state should you be in to start? and then generally states lead to other states.
     if (trafficlight == Green) begin
         state <= Green;
     end
@@ -37,7 +37,7 @@ always @(posedge clk) begin
         state <= Red;
     end
 
-    if (addressLoad == ) begin 
+    if (addressLoad == ) begin
         if (counter == 8) begin
             counter = 0;
         end
@@ -46,7 +46,7 @@ always @(posedge clk) begin
         end
     end
 
-    
+
     case (state)
         //driving - follow traffic laws
         CSLow: begin
@@ -61,7 +61,7 @@ always @(posedge clk) begin
             ADDR_WE = 1;
             SR_WE = 0;
         end
-        
+
         CSHigh: begin
             MISO_BUFE = 0;
             DM_WE = 0;
