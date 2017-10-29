f72307624a6b95e852e6f32f26495bc6636eb8a0
dpapp <david.papp@students.olin.edu>
2017-10-29 18:34:01 -0400

    spi

--- spimemory.v
+++ spimemory.v
@@ -3,6 +3,8 @@
 //------------------------------------------------------------------------
 `include "inputconditioner.v"
 `include "shiftregister.v"
+`include "dff.v"
+`include "datamemory.v"
 
 module spiMemory
 (
@@ -12,21 +14,23 @@ module spiMemory
     output          miso_pin,   // SPI master in slave out
     input           mosi_pin,   // SPI master out slave in
     output [3:0]    leds        // LEDs for debugging
-)
+);
+	reg MISO_BUFE, DM_WE, ADDR_WE, SR_WE;
 	wire conditionedInput1, conditionedInput2;
 	wire positiveEdge, negativeEdge;
-	inputcondition condition1(clk, miso_pin, conditioned, ,);
-	inputcondition condition1(clk, sclk_pin, , positiveEdge, negativeEdge);
-	inputcondition condition1(clk, cs_pin, conditionedInput2, , );
+	inputconditioner condition1(clk, miso_pin, conditioned, ,);
+	inputconditioner condition2(clk, sclk_pin, , positiveEdge, negativeEdge);
+	inputconditioner condition3(clk, cs_pin, conditionedInput2, , );
 
-	wire parallelOut[7:0], serialOut;
-	shiftregister shiftRegister(clk, positiveEdge, SR_WE, parallelDataIn, conditionedInput1, parallelOut, serialOut);
+	wire shiftRegOutP[7:0], serialOut;
+	wire dataMemoryOut[7:0], address[7:0];
+	shiftregister shiftRegister(clk, positiveEdge, SR_WE, dataMemoryOut, conditionedInput1, shiftRegOutP, serialOut);
 
+	/*dff #(8) dff1(clk, ADDR_WE, shiftRegOutP, address);
 	wire q;
-	dff dFF(clk, serialOut, negativeEdge, q, );
+	dff #(1) dff2(clk, negativeEdge, serialOut, q);
 
-	wire parallelIn[7:0], address[7:0]
-	datamemory dataMemory(clk, parallelIn, address[6:0], DM_WE, parallelOut);
+	datamemory dataMemory(clk, shiftRegOutP, address, DM_WE, dataMemoryOut);*/
 
 
 endmodule
