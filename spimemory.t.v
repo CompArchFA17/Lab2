23dc95f957eb1ae461d5f7de4541c6c8544c61f6
arianaolson419 <arianaolson419@gmail.com>
2017-10-30 18:42:18 -0400

    Change tests

--- spimemory.t.v
+++ spimemory.t.v
@@ -12,96 +12,104 @@ module testspimemory ();
 	initial clk = 0;
 	always #10 clk = !clk;
 
+	initial sclk = 0;
+	always #250 sclk = !sclk;
+
 	initial begin
 		$dumpfile("spimemory.vcd");
 		$dumpvars();
 
 // Address bits
-		cs = 1;
-
+		cs = 1; #500
 		cs = 0;
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+		mosi_pin = 0; #3500
+		mosi_pin = 1; #500
+		mosi_pin = 8; #4000
+		cs  = 1; #500
+		cs = 0;
+		mosi_pin = 0; #8000;		
+		// sclk = 1; #15
+		// mosi_pin = 0;
+		// sclk = 0; #15
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+		// sclk = 1; #15
+		// // mosi_pin = 0;
+		// sclk = 0; #15
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+		// sclk = 1; #15
+		// // mosi_pin = 0;
+		// sclk = 0; #15
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+		// sclk = 1; #15
+		// // mosi_pin = 0;
+		// sclk = 0; #15
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+		// sclk = 1; #15
+		// // mosi_pin = 0;
+		// sclk = 0; #15
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+		// sclk = 1; #15
+		// // mosi_pin = 0;
+		// sclk = 0; #15
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+		// sclk = 1; #15
+		// // mosi_pin = 0;
+		// sclk = 0; #15
 
 		
-// Read/Write flag
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// // Read/Write flag
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-// Data bits
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// // Data bits
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-		$displayb(miso_pin);
+// 		$displayb(miso_pin);
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-		$displayb(miso_pin);
+// 		$displayb(miso_pin);
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-		$displayb(miso_pin);
+// 		$displayb(miso_pin);
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-		$displayb(miso_pin);
+// 		$displayb(miso_pin);
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-		$displayb(miso_pin);
+// 		$displayb(miso_pin);
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-		$displayb(miso_pin);
+// 		$displayb(miso_pin);
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-		$displayb(miso_pin);
+// 		$displayb(miso_pin);
 
-		sclk = 1; #15
-		mosi_pin = 0;
-		sclk = 0; #15
+// 		sclk = 1; #15
+// 		mosi_pin = 0;
+// 		sclk = 0; #15
 
-		$displayb(miso_pin);
+// 		$displayb(miso_pin);
 
 		$finish();
 		
