3479bfffa3042428edaf2e968830d492656934cb
mjakus <margaret.jakus@students.olin.edu>
2017-10-30 20:45:11 -0400

    Working on spimemory

--- spimemory.v
+++ spimemory.v
@@ -2,6 +2,12 @@
 // SPI Memory
 //------------------------------------------------------------------------
 
+`timescale 1 ns / 1 ps
+`include "FSM.v"
+`include "inputconditioner.v"
+`include "shiftregister.v"
+`include "datamemory.v"
+
 module spiMemory
 (
     input           clk,        // FPGA clock
@@ -12,7 +18,31 @@ module spiMemory
     output [3:0]    leds        // LEDs for debugging
 )
 
+// MOSI conditioned variables
+wire MOSI;
+wire positiveedge0;
+wire negativeedge0;
 
+// SCLK conditioned variables
+wire conditioned1;
+wire SCLKEdge;
+wire negativeedge1;
+
+// Chip Select conditioned variables
+wire ChipSel;
+wire positiveedge2;
+wire negativeedge2;
+
+inputconditioner serialInCond(clk, mosi_pin, MOSI, positiveedge0, negativeedge0); // MOSICond is cleaned up MOSI / Serial In
+
+inputconditioner SCLKCond(clk, sclk_pin, conditioned1, SCLKEdge, negativeedge1); // positive edge is your cleaned up SCLKEdge
+
+inputconditioner ChipSelCond(clk, cs_pin, ChipSel, positiveedge2, negativeedge2); // conditioned2 is your cleaned up Chip Select
+
+// need: DFF for MISO, DFF for ADDR, DataMem, FSM
+
+
+shiftregister SPIShift(clk, SCLKEdge, SR_WR, dataMemOut, MOSI, shiftRegOutP, MISO); 
 
 endmodule
    
