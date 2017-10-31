d0d87b05115df68ebbf34be6363d208d96e3ae28
apan64 <andrew.pan@students.olin.edu>
2017-10-31 19:21:41 -0400

    changed fsm to rely on clk

--- fsm.v
+++ fsm.v
@@ -3,6 +3,7 @@ module fsm
 input 	    shiftRegOut,            //defines the state (read or write)
 input	    CS,						//chip select
 input 		sclk,    			//serial clock
+input clk, // system clock
 output reg	    MISOBUFE,    // controls output to MISO
 output reg 	    DM_WE,   // Write enable data memory
 output reg      ADDR_WE,    //  address write enable
@@ -20,75 +21,77 @@ output reg		SR_WE //Parallel Load
   // 6 - first write state: accepting data - need to accept 8 more data bits
   // 7 - writing to DM: sets everything to zero once writing is done
     
-  always @(posedge sclk) begin
-    case(currentState) 
-      0: begin // Default State
-        if (CS == 0) begin
-          currentState <= 1;
+  always @(posedge clk) begin
+    if (sclk) begin
+      case(currentState) 
+        0: begin // Default State
+          if (CS == 0) begin
+            currentState <= 1;
+          end
+          else begin
+            // we need to set everything to zero here just in case
+            MISOBUFE <= 0;
+            DM_WE <= 0;
+            ADDR_WE <= 0;
+            SR_WE <=0;
+            counter <= 0;
+          end
         end
-        else begin
-          // we need to set everything to zero here just in case
-          MISOBUFE <= 0;
-          DM_WE <= 0;
-          ADDR_WE <= 0;
-          SR_WE <=0;
-          counter <= 0;
-        end
-      end
-      
-      1: begin // Accepting Address
-        ADDR_WE <=1;
-        counter <= counter + 1;
-        if(counter == 6) begin
-        	currentState <= 2;
-          counter <= 0; //reset counter
-        end
-      end
-    
-      2: begin // Accepting Read/Write Bit
-        ADDR_WE <= 0;
-        if (shiftRegOut == 1) begin
-        	currentState <= 3;
-        end
-        else begin
-        	currentState <= 6;
-        end
-      end
-      
-      3: begin // First read state, set WE high
-        SR_WE <= 1;
-        currentState <= 4; 
-      end
-      
-      4: begin // Second read state, set WE low
-      	SR_WE <= 0;
-        currentState <= 5;
-      end
-      
-      5: begin 
-        MISOBUFE <= 1;
-        counter <= counter + 1; 
-        if (counter == 7) begin
-          currentState <= 0;
-          counter <= 0;
-        end
-      end
-      
-      6: begin // allowing shift register to accept 8 bits of data
-        if (counter == 7) begin
-          DM_WE <= 1;
-          currentState <= 7;
-          counter <= 0;
-        end
-        else begin
+        
+        1: begin // Accepting Address
+          ADDR_WE <=1;
           counter <= counter + 1;
+          if(counter == 6) begin
+          	currentState <= 2;
+            counter <= 0; //reset counter
+          end
         end
-      end
       
-      7: begin // writing to DM for one clock cycle (DM is already high by the time this is reached)
-        DM_WE <= 0;
-        currentState <= 0;
-      end
-    endcase
+        2: begin // Accepting Read/Write Bit
+          ADDR_WE <= 0;
+          if (shiftRegOut == 1) begin
+          	currentState <= 3;
+          end
+          else begin
+          	currentState <= 6;
+          end
+        end
+        
+        3: begin // First read state, set WE high
+          SR_WE <= 1;
+          currentState <= 4; 
+        end
+        
+        4: begin // Second read state, set WE low
+        	SR_WE <= 0;
+          currentState <= 5;
+        end
+        
+        5: begin 
+          MISOBUFE <= 1;
+          counter <= counter + 1; 
+          if (counter == 7) begin
+            currentState <= 0;
+            counter <= 0;
+          end
+        end
+        
+        6: begin // allowing shift register to accept 8 bits of data
+          if (counter == 7) begin
+            DM_WE <= 1;
+            currentState <= 7;
+            counter <= 0;
+          end
+          else begin
+            counter <= counter + 1;
+          end
+        end
+        
+        7: begin // writing to DM for one clock cycle (DM is already high by the time this is reached)
+          DM_WE <= 0;
+          currentState <= 0;
+        end
+      endcase
+    end
   end
 endmodule
