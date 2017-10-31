7eed09b6b49f409eb88dd8a56b78b82b77ebcd41
Kimber <kimberly.winter@students.olin.edu>
2017-10-30 23:08:43 -0400

    implemented my part

--- fsm.v
+++ fsm.v
@@ -2,15 +2,24 @@ module inputconditioner
 (
 input 	    shiftRegOut,            //defines the state (read or write)
 input	    CS,						//chip select
-input 		sclockedge,    			//serial clock
+input 		sclk,    			//serial clock
 output	    MISOBUFE,    // controls output to MISO
 output 	    DM_WE,   // Write enable data memory
 output      ADDR_WE,    //  address write enable
 output		SR_WE //Parallel Load
 );
+reg counter=0;
 
-always @(!CS) begin
-	
+always @(posedge sclk) begin
+	if(CS==0) begin
+		if(counter<=6) begin
+			assign ADDR_WE=1;
+			counter=counter+1;
+		end
+		else if(counter==7) begin
+			assign ADDR_WE=0;
+		end
+	end
 
 
 end
