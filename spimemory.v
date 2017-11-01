84e06e806bc47d622a05d4f95f8514a97dd375b2
Kimber <kimberly.winter@students.olin.edu>
2017-11-01 17:03:49 -0400

    spimemory

--- spimemory.v
+++ spimemory.v
@@ -1,6 +1,11 @@
 //------------------------------------------------------------------------
 // SPI Memory
 //------------------------------------------------------------------------
+`timescale 1ns / 1ps
+`include "inputconditioner.v"
+`include "fsm.v"
+`include "datamemory.v"
+`include "shiftregister.v"
 
 module spiMemory
 (
@@ -10,8 +15,56 @@ module spiMemory
     output          miso_pin,   // SPI master in slave out
     input           mosi_pin,   // SPI master out slave in
     output [3:0]    leds        // LEDs for debugging
-)
+);
+wire [7:0] shiftRegOutP;
 
+//input conditioners
+inputconditioner ic0(clk,mosi_pin, SerialIn,posedge0,negedge0);
+inputconditioner ic1(clk,sclk_pin, cond0, ClkEdge, CEDFF);
+inputconditioner ic2(clk,cs_pin, cs, posedge1,negedge1);
+
+//FSM
+fsm fsm0(shiftRegOutP[0],cs,ClkEdge,clk,MISOBUFE,DM_WE,ADDR_WE,SR_WE);
+
+//shift register
+shiftregister sr0(clk,ClkEdge,SR_WE,DataMemOut[7:0],SerialIn, shiftRegOutP[7:0],D0);
+
+//data memory
+datamemory dm0(clk,dataMemOut[7:0],address[6:0],DM_WE,shiftRegOutP[7:0]);
+
+//address latch
+addressLatch aL0(ADDR_WE,shiftRegOutP[6:0],clk,address[6:0]);
+
+//dff
+dff dff0(D0,Q0,CEDFF,clk);
+
+//miso buffer
+bufif1(miso_pin,Q0,MISO_BUFE);
 
 endmodule
    
+module addressLatch
+(
+input CE,
+input [6:0]D,
+input C,
+output [6:0]Q
+);
+always @(posedge C) begin
+	if (CE==1)
+	Q[6:0]<=D[6:0];
+end
+endmodule
+
+
+module dff(
+input D,
+output Q,
+input CE,
+input C
+);
+always @(posedge C) begin
+	if (CE==1)
+		begin Q<=D; end
+	end
+endmodule
