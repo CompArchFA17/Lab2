f535a8b5a29f17750646dcd90ea0bb4db18cf901
Kimber <kimberly.winter@students.olin.edu>
2017-11-01 18:14:45 -0400

    no warnings/errors spimemory

--- spimemory.v
+++ spimemory.v
@@ -7,6 +7,32 @@
 `include "datamemory.v"
 `include "shiftregister.v"
 
+module addressLatch
+(
+input CE,
+input [6:0]D,
+input C,
+output reg [6:0]Q
+);
+always @(posedge C) begin
+	if (CE==1)
+	Q[6:0]<=D[6:0];
+end
+endmodule
+
+
+module dff(
+input D,
+output reg Q,
+input CE,
+input C
+);
+always @(posedge C) begin
+	if (CE==1)
+		begin Q<=D; end
+	end
+endmodule
+
 module spiMemory
 (
     input           clk,        // FPGA clock
@@ -17,6 +43,8 @@ module spiMemory
     output [3:0]    leds        // LEDs for debugging
 );
 wire [7:0] shiftRegOutP;
+wire [7:0] DataMemOut;
+wire [6:0] address;
 
 //input conditioners
 inputconditioner ic0(clk,mosi_pin, SerialIn,posedge0,negedge0);
@@ -30,7 +58,7 @@ fsm fsm0(shiftRegOutP[0],cs,ClkEdge,clk,MISOBUFE,DM_WE,ADDR_WE,SR_WE);
 shiftregister sr0(clk,ClkEdge,SR_WE,DataMemOut[7:0],SerialIn, shiftRegOutP[7:0],D0);
 
 //data memory
-datamemory dm0(clk,dataMemOut[7:0],address[6:0],DM_WE,shiftRegOutP[7:0]);
+datamemory dm0(clk,DataMemOut[7:0],address[6:0],DM_WE,shiftRegOutP[7:0]);
 
 //address latch
 addressLatch aL0(ADDR_WE,shiftRegOutP[6:0],clk,address[6:0]);
@@ -41,30 +69,4 @@ dff dff0(D0,Q0,CEDFF,clk);
 //miso buffer
 bufif1(miso_pin,Q0,MISO_BUFE);
 
-endmodule
-   
-module addressLatch
-(
-input CE,
-input [6:0]D,
-input C,
-output [6:0]Q
-);
-always @(posedge C) begin
-	if (CE==1)
-	Q[6:0]<=D[6:0];
-end
-endmodule
-
-
-module dff(
-input D,
-output Q,
-input CE,
-input C
-);
-always @(posedge C) begin
-	if (CE==1)
-		begin Q<=D; end
-	end
 endmodule
