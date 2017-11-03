04328332b32a3943cefa1fbaacb9efe0558a26af
Wilson Tang <wilson.t.tang@gmail.com>
2017-11-02 20:31:09 -0400

    test case 3

--- spimemory.t.v
+++ spimemory.t.v
@@ -22,7 +22,7 @@ module testspimemory();
     	sclk_pin = 0;
 		clk=0;
 	end
-	always #50 clk=!clk;    // 50MHz Clock
+	always #5 clk=!clk;    // 50MHz Clock
 	always #50 sclk_pin=!sclk_pin;
 	
 	initial begin
@@ -32,6 +32,7 @@ module testspimemory();
 
 		//Test Case 1: Write: Address: 00000010 Data: 111111111
 
+		$display ("Initialize Testing");
 		data = 16'b0000001011111111;
 
 		cs_pin = 0; #100
@@ -61,7 +62,36 @@ module testspimemory();
 
 		cs_pin = 1; #600
 
-		// TestCase 2: Read: Address: 00000011 Data: 111111111
+		// TestCase 2: Read: Address: 00000011 
+
+		data = 16'b0000001111111111;
+
+		mosi_pin = data[15]; #100
+		mosi_pin = data[14]; #100
+		mosi_pin = data[13]; #100
+		mosi_pin = data[12]; #100
+		mosi_pin = data[11]; #100
+		mosi_pin = data[10]; #100
+		mosi_pin = data[9]; #1001
+		
+		mosi_pin = data[8]; #100 
+		
+		if ((miso_pin != data[7])) begin $display ("Test Failed at Read Element 1: %b ", miso_pin); end #100
+		if ((miso_pin != data[6])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != data[5])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != data[4])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != data[3])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != data[2])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != data[1])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != data[0])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		//checkSPI spi_case2(.data(16'b0000001111111111),.clk(clk),.slck(slck),.cs_pin(cs_pin),.mosi_pin(mosi_pin));
+
+		cs_pin = 0; #100
+
+		// TestCase 3: Read other Address: 000000101 
+		// Check to make sure that our Write didnt write to other parts
+
+		data = 16'b0000010111111111;
 
 		mosi_pin = data[15]; #100
 		mosi_pin = data[14]; #100
@@ -73,18 +103,22 @@ module testspimemory();
 		
 		mosi_pin = data[8]; #100 
 		
-		if ((miso_pin != data[7])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
-		if ((miso_pin != data[6])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
-		if ((miso_pin != data[5])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
-		if ((miso_pin != data[4])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
-		if ((miso_pin != data[3])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
-		if ((miso_pin != data[2])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
-		if ((miso_pin != data[1])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
-		if ((miso_pin != data[0])) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
-		//checkSPI spi_case2(.data(16'b0000001111111111),.clk(clk),.slck(slck),.cs_pin(cs_pin),.mosi_pin(mosi_pin));
+		$display ("Testing ongoing");
+		if ((miso_pin != 0)) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != 0)) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != 0)) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != 0)) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != 0)) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != 0)) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != 0)) $display ("Test Failed at Read Element 1: %b ", miso_pin); #100
+		if ((miso_pin != 1)) begin $display ("Test Failed at Read Element 1: %b ", miso_pin); end #100
 
+		//checkSPI spi_case2(.data(16'b0000001111111111),.clk(clk),.slck(slck),.cs_pin(cs_pin),.mosi_pin(mosi_pin));
 		#100 $finish;
 	end
 
 endmodule
+
+
+
 
